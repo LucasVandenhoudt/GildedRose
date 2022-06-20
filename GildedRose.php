01ce25e81decd4ab6e0b4b11e543ae9453a1f234
LucasVandenhoudt <107849761+LucasVandenhoudt@users.noreply.github.com>
2022-06-20 15:25:07 +0200

    Add files via upload

---
+++ GildedRose.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose;
+
+final class GildedRose
+{
+    const AGEDBRIE = 'Aged Brie';
+    const CONJURED = 'Conjured';
+    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+    const ERREUR = 'error';
+    const QUALITEMAX = 50;
+    const QUALITEMIN = 0;
+    const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+    const SELLINMIN=0;
+    const NOMBRE_UPDATE_QUALITY_CONJURED = 2;
+    const NOMBRE_UPDATE_QUALITY_DEFAULT = 1;
+    const BACKSTAGE_INFERIEUR_OU_EGALE_A10 = 10;
+    const BACKSTAGE_INFERIEUR_OU_EGALE_A5 = 5;
+    /**
+     * @var Item[]
+     */
+    private $items;
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    public function updateQuality(): void
+    {
+        foreach ($this->items as $item) {
+            $this->updateQualityOneItem($item);
+        }
+    }
+
+    
+    private function updateQualityOneItem(Item $item):void
+    {
+        $this->updateSellInPositif($item);
+        $this->updateSellIn($item);
+        $this->updateSellInNegatif($item);
+    }
+
+    private function updateQualityBackstageOrAgedBrie(Item $item):void
+    {
+        if ($item->name == self::BACKSTAGE) {
+            if ($item->sell_in <= self::BACKSTAGE_INFERIEUR_OU_EGALE_A10) {
+                if ($item->quality < self::QUALITEMAX) {
+                    $item->quality = $item->quality + self::NOMBRE_UPDATE_QUALITY_DEFAULT;
+                }
+            }
+            if ($item->sell_in <= self::BACKSTAGE_INFERIEUR_OU_EGALE_A5) {
+                if ($item->quality < self::QUALITEMAX) {
+                    $item->quality = $item->quality + self::NOMBRE_UPDATE_QUALITY_DEFAULT;
+                }
+            }
+        }
+    }
+
+    private function updateSellInNegatif(Item $item):void
+    {
+
+        if ($item->sell_in < self::SELLINMIN) {
+            if ($item->name == self::CONJURED) {
+                $item->quality = $item->quality - self::NOMBRE_UPDATE_QUALITY_CONJURED;
+            }
+            if ($item->name == self::AGEDBRIE) {
+                if ($item->quality < self::QUALITEMAX) {
+                    $item->quality = $item->quality + self::NOMBRE_UPDATE_QUALITY_DEFAULT;
+                }
+            } else {
+                $this->updateQualityIfSellInNegatif($item);
+            }
+        }
+    }
+
+    private function updateSellInPositif(Item $item):void
+    {
+        if ($this->checkItemNameDifferentAgedBrieBackstageConjured($item)) {
+            $this->updateQualityIfSellInPositif($item);
+        } else {
+            if ($item->quality < self::QUALITEMAX) {
+                $item->quality = $item->quality + self::NOMBRE_UPDATE_QUALITY_DEFAULT;
+                $this->updateQualityBackstageOrAgedBrie($item);
+            }
+        }
+    }
+
+    private function checkItemNameDifferentAgedBrieBackstageConjured(Item $item):bool
+    {
+        return $item->name != self::AGEDBRIE and $item->name != self::BACKSTAGE and $item->name != self::CONJURED;
+    }
+
+    private function updateSellIn(Item $item):void
+    {
+        if ($item->name != self::SULFURAS) {
+            $item->sell_in = $item->sell_in - self::NOMBRE_UPDATE_QUALITY_DEFAULT;
+        }
+    }
+
+    private function updateQualityIfSellInNegatif(Item $item):void
+    {
+        if ($item->name != self::BACKSTAGE) {
+            if ($item->quality > self::QUALITEMIN) {
+                if ($item->name != self::SULFURAS) {
+                    $item->quality = $item->quality - self::NOMBRE_UPDATE_QUALITY_DEFAULT;
+                }
+            }
+        } else {
+            $item->quality = 0;
+        }
+    }
+
+    private function updateQualityIfSellInPositif(Item $item):void
+    {
+        if ($this->checkQualityEntre0Et50($item)) {
+            if ($item->name != self::SULFURAS) {
+                $item->quality = $item->quality - self::NOMBRE_UPDATE_QUALITY_DEFAULT;
+            }
+            if ($item->name== self::CONJURED) {
+                $item->quality = $item->quality - self::NOMBRE_UPDATE_QUALITY_CONJURED;
+            }
+        } else {
+            $item->quality = self::ERREUR;
+        }
+    }
+
+    private function checkQualityEntre0Et50(Item $item):bool
+    {
+            return $item->quality > self::QUALITEMIN and $item->quality < self::QUALITEMAX;
+    }
+}
---
+++ GildedRoseTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use GildedRose\GildedRose;
+use GildedRose\Item;
+use PHPUnit\Framework\TestCase;
+
+class GildedRoseTest extends TestCase
+{
+    /*test d'acceptation sur l'erreur si la qualité est négative*/
+    public function testQualiteNegative(): void
+    {
+        $items = [new Item('Produit', 5, -1)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame('error', $items[0]->quality);
+    }
+
+    /*test d'acceptation sur l'augmentation de la qualité de l'objet Aged Brie*/
+    public function testQualiteAgedBrie(): void
+    {
+        $items = [new Item('Aged Brie', -1, 45)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(47, $items[0]->quality);
+    }
+
+    /*test d'acceptation sur l'erreur si la qualité est supérieur à 50*/
+    public function testQualiteSupperieur50(): void
+    {
+        $items = [new Item('Produit', 5, 51)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame('error', $items[0]->quality);
+    }
+
+    /*test d'acceptation sur la qualité de l'objet Sulfura*/
+    public function testSulfuraQuality(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 10, 30)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(30, $items[0]->quality);
+    }
+
+    /*test d'acceptation sur le sellIn de l'objet Sulfura*/
+    public function testSulfuraSellIn(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 10, 30)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(10, $items[0]->sell_in);
+    }
+
+    /*test d'acceptation sur la qualité de l'objet Backstage quand il est compris entre 5 et 9*/
+    public function testBackstageInferieur10(): void
+    {
+        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 9, 30)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(32, $items[0]->quality);
+    }
+
+    /*test d'acceptation sur la qualité de l'objet Backstage quand il est compris entre 1 et 4*/
+    public function testBackstageInferieur5(): void
+    {
+        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 4, 30)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(33, $items[0]->quality);
+    }
+    
+    /*test d'acceptation sur la qualité de l'objet Backstage quand il est négatif*/
+    public function testBackstageNegatif(): void
+    {
+        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', -1, 30)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $items[0]->quality);
+    }
+
+    /*test d'acceptation sur la qualité d'un objet crée dont la qualité est inférieur à 50*/
+    public function testProduit(): void
+    {
+        $items = [new Item('Produit', 3, 29)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(28, $items[0]->quality);
+    }
+
+    /*test d'acceptation sur le sellIn d'un objet crée dont le sellIn est négatif*/
+    public function testProduitQualite(): void
+    {
+        $items = [new Item('Produit', -1, 29)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(27, $items[0]->quality);
+    }
+
+    /*test d'acceptation sur la qualité d'un objet Conjured dont le sellIn est négatif*/
+    public function testQualiteConjured(): void
+    {
+        $items = [new Item('Conjured', -1, 44)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(42, $items[0]->quality);
+    }
+}
