3fad2bd3d93bbe0052d452524fb356d7fcabdcad
LucasVandenhoudt <107849761+LucasVandenhoudt@users.noreply.github.com>
2022-06-22 08:16:45 +0200

    Update GildedRoseQualityIssue.php

--- GildedRoseQualityIssue.php
+++ GildedRoseQualityIssue.php
@@ -6,26 +6,8 @@ namespace GildedRose;
 
 use GildedRose\Item;
 
-final class GildedRoseQualityIssue
+final class GildedRoseQualityIssue extends GildedRose
 {
-    const AGEDBRIE = 'Aged Brie';
-    const CONJURED = 'Conjured';
-    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
-    const ERREUR = 'error';
-    const QUALITEMAX = 50;
-    const QUALITEMIN = 0;
-    const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
-    const SELLINMIN=0;
-    const NOMBRE_UPDATE_QUALITY_CONJURED = 2;
-    const NOMBRE_UPDATE_QUALITY_DEFAULT = 1;
-    const BACKSTAGE_INFERIEUR_OU_EGALE_A10 = 10;
-    const BACKSTAGE_INFERIEUR_OU_EGALE_A5 = 5;
-    const NOMBRE_UPDATE_SELLIN_DEFAULT=1;
-
-    public function __construct(array $items)
-    {
-        $this->items = $items;
-    }
 
     public function updateQuality():void
     {
@@ -42,9 +24,10 @@ final class GildedRoseQualityIssue
 
     private function augmenterUn(Item $item):bool
     {
-        if($item->name == self::BACKSTAGE && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A10 && $item->quality < self::QUALITEMAX ||
-        $item->name == self::AGEDBRIE && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A10 && $item->quality < self::QUALITEMAX)
-        {
+        if ($item->name == self::BACKSTAGE && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A10
+        && $item->quality < self::QUALITEMAX ||
+        $item->name == self::AGEDBRIE && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A10
+        && $item->quality < self::QUALITEMAX) {
             $item->quality++;
             return true;
         }
@@ -53,8 +36,11 @@ final class GildedRoseQualityIssue
 
     private function augmenterDeux(Item $item):bool
     {
-        if ($item->name==self::BACKSTAGE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A10 && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A5 && $item->sell_in>self::SELLINMIN && $item->quality<self::QUALITEMAX || $item->name==self::AGEDBRIE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A10 && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A5 && $item->sell_in>self::SELLINMIN && $item->quality<self::QUALITEMAX) 
-        {
+        if ($item->name==self::BACKSTAGE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A10
+        && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A5 && $item->quality<self::QUALITEMAX
+        || $item->name==self::AGEDBRIE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A10
+        && $item->sell_in>self::BACKSTAGE_INFERIEUR_OU_EGALE_A5
+        && $item->quality<self::QUALITEMAX) {
             $item->quality= $item->quality + 2;
             return true;
         }
@@ -63,8 +49,12 @@ final class GildedRoseQualityIssue
 
     private function augmenterTrois(Item $item):bool
     {
-        if ($item->name==self::BACKSTAGE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A5 && $item->sell_in>self::SELLINMIN && $item->quality<self::QUALITEMAX || $item->name==self::AGEDBRIE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A5 && $item->sell_in>self::SELLINMIN && $item->sell_in>self::SELLINMIN && $item->quality<self::QUALITEMAX)
-        {
+        if ($item->name==self::BACKSTAGE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A5
+        && $item->sell_in>self::SELLINMIN
+        && $item->quality<self::QUALITEMAX
+        || $item->name==self::AGEDBRIE && $item->sell_in<=self::BACKSTAGE_INFERIEUR_OU_EGALE_A5
+        && $item->sell_in>self::SELLINMIN
+        && $item->quality<self::QUALITEMAX) {
             $item->quality= $item->quality + 3;
             return true;
         }
@@ -73,9 +63,10 @@ final class GildedRoseQualityIssue
 
     private function enleverUn(Item $item):bool
     {
-        if($item->name != self::AGEDBRIE and $item->name != self::BACKSTAGE && $item->quality > self::QUALITEMIN && $item->name != self::SULFURAS && $item->name != self::CONJURED ||
-        $item->sell_in < 0 && $item->name != 'Aged Brie' && $item->name != 'Backstage passes to a TAFKAL80ETC concert' && $item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros' && $item->name != self::CONJURED)
-        {
+        if ($item->name != self::AGEDBRIE and $item->name != self::BACKSTAGE && $item->quality > self::QUALITEMIN
+        && $item->name != self::SULFURAS && $item->name != self::CONJURED ||
+        $item->sell_in < self::SELLINMIN && $item->name != self::AGEDBRIE && $item->name != self::BACKSTAGE
+        && $item->quality > self::QUALITEMIN && $item->name != self::SULFURAS && $item->name != self::CONJURED) {
             $item->quality = $item->quality - 1;
             return true;
         }
@@ -84,8 +75,8 @@ final class GildedRoseQualityIssue
 
     private function enleverDeux(Item $item):bool
     {
-        if ($item->name==self::CONJURED && $item->quality<self::QUALITEMAX && $item->sell_in>self::SELLINMIN || $item->sell_in<self::SELLINMIN && $item->name==self::CONJURED && $item->quality<self::QUALITEMAX) 
-        {
+        if ($item->name==self::CONJURED && $item->quality<self::QUALITEMAX && $item->sell_in>self::SELLINMIN
+        || $item->sell_in<self::SELLINMIN && $item->name==self::CONJURED && $item->quality<self::QUALITEMAX) {
             $item->quality= $item->quality - 2;
             return true;
         }
@@ -94,8 +85,7 @@ final class GildedRoseQualityIssue
 
     private function updateSellIn(Item $item):bool
     {
-        if($item->name != self::SULFURAS)
-        {
+        if ($item->name != self::SULFURAS) {
             $item->sell_in = $item->sell_in - self::NOMBRE_UPDATE_SELLIN_DEFAULT;
             return true;
         }
