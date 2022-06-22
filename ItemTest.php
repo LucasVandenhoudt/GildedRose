af35315512c84abb122d920920c03b6016843012
LucasVandenhoudt <107849761+LucasVandenhoudt@users.noreply.github.com>
2022-06-22 08:19:15 +0200

    Update ItemTest.php

--- ItemTest.php
+++ ItemTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use GildedRose\GildedRose;
 use GildedRose\Item;
 use PHPUnit\Framework\TestCase;
 
@@ -13,8 +12,8 @@ class ItemTest extends TestCase
     /*test d'acceptation sur la méthode toString pour vérifier l'affichage*/
     public function testToString():void
     {
-        $this->items = new Item('Conjured', 15, 9);
-        $resultat=$this->items->__toString();
+        $item = new Item('Conjured', 15, 9);
+        $resultat=$item->__toString();
         $this->assertSame("Conjured, 15, 9", $resultat);
     }
 }
